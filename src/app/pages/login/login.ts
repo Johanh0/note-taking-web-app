6cfcaa392ef23c5ef62638e42adb692bf3d73c99
Johanh0 <johanherrera20000@gmail.com>
2026-02-18 21:29:29 -0500

    main login styles done

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,14 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { Theme as ThemeService } from '../../services/theme';
+import { Login as LoginType } from '../../models/login';
+import { form, required, email, validate, FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-login',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, FormField],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
 
   protected themeService = inject(ThemeService);
+
+  loginModel = signal<LoginType>({
+    email: '',
+    password: ''
+  });
+
+  loginForm = form(this.loginModel)
+
 }
